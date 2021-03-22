1f306dde67d706fb6daeefd8ed3fec672157c2fc
Ubuntu <ubuntu@ip-172-31-46-117.eu-west-1.compute.internal>
2021-03-22 13:55:42 +0000

    Hello world controller create

---
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -0,0 +1,11 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalTrackerApplication {
+    public static void main(String[] a){
+        SpringApplication.run(PalTrackerApplication.class);
+    }
+}
